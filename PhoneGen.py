c0d4dd527a94640972691444a2af4a4a927bae5c
R4nd0mbyt3 <reyes.viento@gmail.com>
2016-08-06 12:50:28 -0500

    Update PhoneGen.py

--- PhoneGen.py
+++ PhoneGen.py
@@ -1,10 +1,10 @@
-print("Bienvenido a PhoneKeyGenerator, Creado por Wlan0")
+print'NumberPhone-Generator v.2.1----------- Creado por R4nd0mByt3'
 def Fun1():
     lada1=raw_input("ingrese la lada principal: ")
     lada2=raw_input("ingrese la lada secundaria: ")
     numero=int(0000)
-    print("la lada principal es: ",lada1," y la lada secundaria es: ",lada2)
-    decision=raw_input("presione Y si desea continuar, o presione N para corregir: ")
+    print 'la lada principal es: ',lada1,' y la lada secundaria es: ',lada2
+    decision=raw_input("presione Y si desea continuar, o presione N para corregir: ").upper()
     if (decision=='Y'):
         print("generando diccionario, porfavor espere....")
         while(numero<=9999):
